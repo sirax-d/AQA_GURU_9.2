1df827d3c8f579f4acb46e20188975414c1cdc5e
Krichanov Sergey <dsirax@icloud.com>
2023-11-12 10:39:03 +0100

    add repos

---
+++ test_google_should_find_selene.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+from selene.support.shared import browser, config
+from selene import be, have
+
+@pytest.fixture
+def start_settings_google():
+    browser.open('https://google.com')
+    config.window_width = 800
+    config.window_height = 1200
+    browser.open('https://google.com')
+    yield
+    browser.clear_local_storage()
+    browser.quit()
+
+#
+# # def test_one(start_settings):
+#     browser.element('[id="L2AGLb"]').click()
+#     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+#     # browser.element('[name="body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf.h3L8Ub.emcav > div.UUbT9.EyBRub > div.aajZCb > div.lJ9FBc > center > input.gNO89b"]').click()
+#     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+def test_empty_results(start_settings_google):
+    browser.element('[id="L2AGLb"]').click() #Согласие на куки
+    browser.element('[name="q"]').should(be.blank).type('wejrio3roi32or34r3jof32342') #Находим input поле поиска
+    browser.element('.FPdoLc input.gNO89b').click() #Нажимаем найти
+    browser.element('[id="botstuff"]').should(have.text('По запросу wejrio3roi32or34r3jof32342 ничего не найдено.')) #Проверяем что результатов нету
