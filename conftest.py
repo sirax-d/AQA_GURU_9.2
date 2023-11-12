5ce92e60e2676784a4b5a3ce758a356ac0bdf1a3
Krichanov Sergey <dsirax@icloud.com>
2023-11-12 11:00:02 +0100

    fix размеров окна, в конфтесте

--- conftest.py
+++ conftest.py
@@ -3,9 +3,8 @@ from selene.support.shared import browser, config
 
 @pytest.fixture(scope="module")
 def start_settings_google():
-    browser.open('https://google.com')
-    config.window_width = 800
-    config.window_height = 1200
+    config.window_width = 1600
+    config.window_height = 1280
     browser.open('https://google.com')
     yield
     browser.clear_local_storage()
